25dfc31cd386a18b5fac7c246a41f3a17c66d1a6
Noa-beluga <jnoat92@gmail.com>
2024-12-19 09:33:50 -0500

    MSELossWithIgnoreIndex

--- mmseg/models/losses/mse_loss.py
+++ mmseg/models/losses/mse_loss.py
@@ -1,62 +1,41 @@
 """
 No@
 August 26th, 2024
-Incomplete
 """
 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 from ..builder import LOSSES
-from .utils import weighted_loss
 
-
-@weighted_loss
-def mse_loss(pred, target):
-    """Warpper of mse loss."""
-    return F.mse_loss(pred, target, reduction='none')
-
-
+# only applicable to regression outputs
 @LOSSES.register_module()
-class MSELoss(nn.Module):
-    """MSELoss.
-
-    Args:
-        reduction (str, optional): The method that reduces the loss to a
-            scalar. Options are "none", "mean" and "sum".
-        loss_weight (float, optional): The weight of the loss. Defaults to 1.0
-    """
-
-    def __init__(self, reduction='mean', loss_weight=1.0):
+class MSELossWithIgnoreIndex(nn.Module):
+    def __init__(self, 
+                 loss_name='loss_mse',
+                 loss_weight = 1.0):
         super().__init__()
-        self.reduction = reduction
         self.loss_weight = loss_weight
+        self._loss_name = loss_name
 
-    def forward(self,
-                pred,
-                target,
-                weight=None,
-                avg_factor=None,
-                reduction_override=None):
-        """Forward function of loss.
+    def forward(self, cls_score, label, ignore_index=255, weight=None):
+        mask = (label != ignore_index).type_as(cls_score)
+        diff = cls_score.squeeze(1) - label
+        diff = diff * mask
+        loss = torch.sum(diff ** 2) / mask.sum()
+        return self.loss_weight * loss
 
-        Args:
-            pred (torch.Tensor): The prediction.
-            target (torch.Tensor): The learning target of the prediction.
-            weight (torch.Tensor, optional): Weight of the loss for each
-                prediction. Defaults to None.
-            avg_factor (int, optional): Average factor that is used to average
-                the loss. Defaults to None.
-            reduction_override (str, optional): The reduction method used to
-                override the original reduction method of the loss.
-                Defaults to None.
+    @property
+    def loss_name(self):
+        """Loss Name.
+
+        This function must be implemented and will return the name of this
+        loss function. This name will be used to combine different loss items
+        by simple sum operation. In addition, if you want this loss item to be
+        included into the backward graph, `loss_` must be the prefix of the
+        name.
 
         Returns:
-            torch.Tensor: The calculated loss
+            str: The name of this loss item.
         """
-        assert reduction_override in (None, 'none', 'mean', 'sum')
-        reduction = (
-            reduction_override if reduction_override else self.reduction)
-        loss = self.loss_weight * mse_loss(
-            pred, target, weight, reduction=reduction, avg_factor=avg_factor)
-        return loss
+        return self._loss_name
